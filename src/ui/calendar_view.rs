c1f4e9db83f432bc59e0927a037d5820c8e34bb7
reedts <j.reedts@gmail.com>
2020-03-27 17:29:01 +0100

    Revert "Use 'grid' layout instead of table"
    This reverts commit e0646b5e7ac2fb6163cde9d37c63021558f37466.

--- src/ui/calendar_view.rs
+++ src/ui/calendar_view.rs
@@ -1,102 +1,47 @@
-use crate::calendar::{Calendar, Day, Month};
-
-use chrono::Utc;
+use crate::calendar::Calendar;
 
 use tui::buffer::Buffer;
-use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
+use tui::layout::{Constraint, Rect};
 use tui::style::{Color, Style};
-use tui::widgets::{Block, Borders, Text, Table, Row, Paragraph, Widget};
-
-struct DayBlock<'a> {
-    day: &'a Day<'a, Utc>
-}
+use tui::widgets::{Block, Borders, Table, Row, Widget};
 
 pub struct CalendarView<'a> {
-    calendar: &'a Calendar<'a>,
-    selection: &'a Month<'a, Utc>,
-    day_blocks: Vec<DayBlock<'a>>
+    calendar: &'a mut Calendar<'a>
 }
 
 impl<'a> CalendarView<'a> {
     pub fn new(calendar: &'a mut Calendar<'a>) -> CalendarView<'a> {
-        let curr_month = calendar.curr_month();
         CalendarView {
-            calendar,
-            selection: curr_month,
-            day_blocks: curr_month.days().iter()
-                .map(|d| DayBlock { day: d })
-                .collect()
+            calendar
         }
-    }
-}
-
-impl<'a> Widget for DayBlock<'a> {
-    fn draw(&mut self, area: Rect, buf: &mut Buffer) {
-        Paragraph::new([Text::raw("test")].iter())
-            .block(Block::default().borders(Borders::ALL))
-            .alignment(Alignment::Center)
-            .draw(area, buf);
     }
 }
 
 impl<'a> Widget for CalendarView<'a> {
     fn draw(&mut self, area: Rect, buf: &mut Buffer) {
-        let rows = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([
-                Constraint::Length(2),
-                Constraint::Length(5),
-                Constraint::Length(5),
-                Constraint::Length(5),
-                Constraint::Length(5),
-                Constraint::Length(5),
-            ].as_ref())
-            .margin(0)
-            .split(Rect {
-                x: area.x,
-                y: area.y,
-                width: 70,
-                height:27
-            });
-        
-        let mut rows: Vec<Vec<Rect>> = rows.iter().map(|r| {
-            Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([
-                    Constraint::Length(10),
-                    Constraint::Length(10),
-                    Constraint::Length(10),
-                    Constraint::Length(10),
-                    Constraint::Length(10),
-                    Constraint::Length(10),
-                    Constraint::Length(10)
-                ].as_ref())
-                .margin(0)
-                .split(*r)
-            }).collect();
-
-        let header = [
-            "Mon",
-            "Tue",
-            "Wed",
-            "Thu",
-            "Fri",
-            "Sat",
-            "Sun"
-        ];
-
-        let header_style = Style::default().fg(Color::Yellow);
-
-        for (col, header) in rows.first_mut().unwrap().iter_mut().zip(header.iter()) {
-            Paragraph::new([Text::styled(*header, header_style)].iter())
-                .alignment(Alignment::Left)
-                .draw(*col, buf);
-        }
-
-        for r in &rows[1..] {
-            for c in r {
-                self.day_blocks.first_mut().unwrap().draw(*c, buf);
-            }
-        }
+        let row_style = Style::default().fg(Color::White);
+        let table = Table::new(
+                ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].iter(),
+                vec![
+                    Row::StyledData(self.calendar.curr_month().days()[0..=7].iter(), row_style),
+                    Row::StyledData(self.calendar.curr_month().days()[8..=14].iter(), row_style),
+                    Row::StyledData(self.calendar.curr_month().days()[15..=22].iter(), row_style),
+                    Row::StyledData(self.calendar.curr_month().days()[22..].iter(), row_style)
+                ].into_iter()
+            )
+            .block(Block::default().title("2020").borders(Borders::ALL))
+            .header_style(Style::default().fg(Color::Yellow))
+            .widths(&[
+                Constraint::Percentage(14),
+                Constraint::Percentage(14),
+                Constraint::Percentage(14),
+                Constraint::Percentage(14),
+                Constraint::Percentage(14),
+                Constraint::Percentage(14),
+                Constraint::Percentage(14)
+            ])
+            .style(Style::default().fg(Color::White))
+            .column_spacing(1)
+            .draw(area, buf);
     }
 }
