d57cbb4a0cb7bc2c58de238c4f19099c9072fc5a
reedts <j.reedts@gmail.com>
2020-12-28 18:38:03 +0100

    Fix timeparsing

--- src/ical/mod.rs
+++ src/ical/mod.rs
@@ -8,14 +8,17 @@ pub use event::Event;
 
 pub type IcalResult<T> = std::result::Result<T, crate::ical::Error>;
 
-use chrono::{DateTime, FixedOffset, NaiveDateTime, Offset, TimeZone, Utc};
+use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime, Offset, TimeZone, Utc};
 use chrono_tz::Tz;
 
 use ::ical::property::Property;
 
+const ISO8601_2004_LOCAL_FORMAT: &str = "%Y%M%DT%H%M%S";
+const ISO8601_2004_LOCAL_FORMAT_DATE: &str = "%Y%M%D";
+
 pub(crate) fn prop_value_in_zulu(timestamp: &Property) -> bool {
     if let Some(v) = &timestamp.value {
-        if v.contains("Z") {
+        if v.contains('Z') {
             return true;
         }
     }
@@ -24,8 +27,6 @@ pub(crate) fn prop_value_in_zulu(timestamp: &Property) -> bool {
 }
 
 pub(crate) fn parse_prop_to_date_time(property: &Property) -> IcalResult<DateTime<FixedOffset>> {
-    const iso8601_2004_local_format: &str = "%Y%M%DT%H%M%S";
-
     let mut found_str_dt: Option<&str> = None;
 
     // Check if property has value
@@ -49,44 +50,57 @@ pub(crate) fn parse_prop_to_date_time(property: &Property) -> IcalResult<DateTim
     // Unpack any parameters
     if let Some(params) = &property.params {
         for param in params {
-            let (name, value) = &param;
-            if name == "TZID" {
-                if let Ok(tz) = value.first().unwrap().parse::<Tz>() {
-                    if let Some(dt) = found_str_dt {
-                        let dt =
-                            NaiveDateTime::parse_from_str(&dt, iso8601_2004_local_format).unwrap();
+            let (name, values) = &param;
+
+            match name.as_str() {
+                "TZID" => {
+                    if let Ok(tz) = values.first().unwrap().parse::<Tz>() {
+                        if let Some(dt) = found_str_dt {
+                            let dt = NaiveDateTime::parse_from_str(&dt, ISO8601_2004_LOCAL_FORMAT)?;
+                            let offset = tz.offset_from_local_datetime(&dt).earliest().unwrap().fix();
+                            return Ok(offset.from_local_datetime(&dt).earliest().unwrap());
+                        }
+                    } else {
+                        return Err(
+                            Error::new(ErrorKind::TimeParse).with_msg("Unable to parse timezone")
+                        );
+                    }
+                },
+                "VALUE" if values.first().unwrap() == "DATE" => {
+                    // Date only
+                    if let Some(date_str) = found_str_dt {
+                        let date =
+                            NaiveDate::parse_from_str(&date_str, ISO8601_2004_LOCAL_FORMAT_DATE)?;
+                        let datetime = date.and_time(NaiveTime::from_hms(0, 0, 0));
                         let offset = Utc
-                            .offset_from_local_datetime(&dt)
+                            .offset_from_local_datetime(&datetime)
                             .earliest()
                             .unwrap()
                             .fix();
-                        return Ok(offset.from_local_datetime(&dt).earliest().unwrap());
+                        return Ok(offset.from_local_datetime(&datetime).earliest().unwrap());
+                    } else {
+                        return Err(Error::new(ErrorKind::EventMissingKey)
+                            .with_msg("Could not find valid timestamp"));
                     }
                 }
-            } else if name == "DATE" {
-                return Err(Error::new(ErrorKind::CalendarMissingKey));
+                _ => return Err(Error::new(ErrorKind::CalendarParse))
             }
         }
-        return Err(Error::new(ErrorKind::CalendarMissingKey));
+    }
+
+    // No parameters found, check if value was found earlier
+    if let Some(dt) = found_str_dt {
+        // Must be localtime (or gibberish)
+        let naive_local = NaiveDateTime::parse_from_str(&dt, ISO8601_2004_LOCAL_FORMAT)?;
+        let offset = Utc
+            .offset_from_local_datetime(&naive_local)
+            .earliest()
+            .unwrap()
+            .fix();
+        return Ok(offset.from_local_datetime(&naive_local).earliest().unwrap());
     } else {
-        // No parameters found, check if value was found earlier
-        if let Some(dt) = found_str_dt {
-            // Must be localtime (or gibberish)
-            if let Ok(naive_local) = NaiveDateTime::parse_from_str(&dt, iso8601_2004_local_format) {
-                let offset = Utc
-                    .offset_from_local_datetime(&naive_local)
-                    .earliest()
-                    .unwrap()
-                    .fix();
-                return Ok(offset.from_local_datetime(&naive_local).earliest().unwrap());
-            } else {
-                return Err(Error::new(ErrorKind::TimeParse)
-                    .with_msg("Failed to parse localtime from iso8601 format"));
-            }
-        } else {
-            return Err(
-                Error::new(ErrorKind::EventMissingKey).with_msg("Could not find valid timestamp")
-            );
-        }
+        return Err(
+            Error::new(ErrorKind::EventMissingKey).with_msg("Could not find valid timestamp")
+        );
     }
 }
