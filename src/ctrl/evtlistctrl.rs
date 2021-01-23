f4ee86d3639abb560a6b957eefb78c5de04458bd
reedts <j.reedts@gmail.com>
2021-01-23 22:20:41 +0100

    Fix out-of-range when switching day selection

--- src/ctrl/evtlistctrl.rs
+++ src/ctrl/evtlistctrl.rs
@@ -51,7 +51,11 @@ impl Selection for EvtListController {
 
     fn move_n_down(&mut self, n: u32, context: &mut Context) {
         let sel_evt = if let Some(item) = context.evtlist_context.selected() {
-            std::cmp::min(item + n as usize, context.get_day().events().len() - 1)
+            if context.get_day().events().is_empty() {
+                0
+            } else {
+                std::cmp::max(item + n as usize, context.get_day().events().len() - 1)
+            }
         } else {
             0
         };
