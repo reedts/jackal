d78ed6e6c4dc5cd4c15a2d4d31fc8abc396c6060
reedts <j.reedts@gmail.com>
2022-01-09 15:00:53 +0100

    Add duration parsing

--- src/ical/calendar.rs
+++ src/ical/calendar.rs
@@ -2,16 +2,23 @@ use chrono::{
     Date, DateTime, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime, Offset, TimeZone, Utc,
 };
 use chrono_tz::Tz;
-use ical::parser::ical::component::IcalCalendar;
-use ical::parser::ical::IcalParser;
 use log;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, digit1, one_of},
+    combinator::{all_consuming, map_res, opt},
+    sequence::{preceded, terminated, tuple},
+    IResult,
+};
 use std::convert::TryFrom;
 use std::fs;
 use std::io;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
-use ical::parser::ical::component::IcalEvent;
+use ical::parser::ical::IcalParser;
+use ical::parser::ical::{component::IcalCalendar, component::IcalEvent};
 use ical::property::Property;
 
 use crate::ical::{Error, ErrorKind};
@@ -20,14 +27,141 @@ use super::{ISO8601_2004_LOCAL_FORMAT, ISO8601_2004_LOCAL_FORMAT_DATE};
 
 #[derive(Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct DurationSpec {
-    dur: chrono::Duration,
+    sign: i8,
+    years: i64,
+    months: i64,
+    weeks: i64,
+    days: i64,
+    hours: i64,
+    minutes: i64,
+    seconds: i64,
+}
+
+impl DurationSpec {
+    fn parse_sign(input: &str) -> IResult<&str, Option<char>> {
+        opt(one_of("+-"))(input)
+    }
+
+    fn parse_integer_value(input: &str) -> std::result::Result<i64, std::num::ParseIntError> {
+        i64::from_str_radix(input, 10)
+    }
+
+    fn value_with_designator(designator: &str) -> impl Fn(&str) -> IResult<&str, i64> + '_ {
+        move |input| {
+            terminated(
+                map_res(digit1, |s: &str| Self::parse_integer_value(s)),
+                tag(designator),
+            )(input)
+        }
+    }
+
+    fn parse_week_format(input: &str) -> IResult<&str, Self> {
+        let (input, weeks) = (Self::value_with_designator("W")(input))?;
+
+        Ok((
+            input,
+            Self {
+                sign: 1,
+                years: 0,
+                months: 0,
+                weeks,
+                days: 0,
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+            },
+        ))
+    }
+
+    fn parse_datetime_format(input: &str) -> IResult<&str, Self> {
+        let (input, (years, months, days)) = tuple((
+            opt(Self::value_with_designator("Y")),
+            opt(Self::value_with_designator("M")),
+            opt(Self::value_with_designator("D")),
+        ))(input)?;
+
+        let (input, time) = opt(preceded(
+            char('T'),
+            tuple((
+                opt(Self::value_with_designator("H")),
+                opt(Self::value_with_designator("M")),
+                opt(Self::value_with_designator("S")),
+            )),
+        ))(input)?;
+
+        let (hours, minutes, seconds) = time.unwrap_or_default();
+
+        if years.is_none()
+            && months.is_none()
+            && days.is_none()
+            && hours.is_none()
+            && minutes.is_none()
+            && seconds.is_none()
+        {
+            Err(nom::Err::Error(nom::error::ParseError::from_error_kind(
+                input,
+                nom::error::ErrorKind::Verify,
+            )))
+        } else {
+            Ok((
+                input,
+                Self {
+                    sign: 1,
+                    years: years.unwrap_or_default(),
+                    months: months.unwrap_or_default(),
+                    weeks: 0,
+                    days: days.unwrap_or_default(),
+                    hours: hours.unwrap_or_default(),
+                    minutes: minutes.unwrap_or_default(),
+                    seconds: seconds.unwrap_or_default(),
+                },
+            ))
+        }
+    }
+
+    fn as_chrono_duration(&self) -> chrono::Duration {
+        chrono::Duration::seconds(
+            self.sign as i64
+                * ((self.years * 12 * 30 * 24 * 60 * 60)
+                    + (self.months * 30 * 24 * 60 * 60)
+                    + (self.weeks * 7 * 24 * 60 * 60)
+                    + (self.hours * 60 * 60)
+                    + (self.minutes * 60)
+                    + (self.seconds)),
+        )
+    }
 }
 
 impl FromStr for DurationSpec {
     type Err = super::error::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        unimplemented!();
+        let (rest, sign) = Self::parse_sign(s)
+            .or_else(|err| {
+                return Err(Self::Err::new(ErrorKind::DurationParse).with_msg(&format!("{}", err)));
+            })
+            .unwrap();
+
+        let (_, mut duration) = (all_consuming(preceded(
+            char('P'),
+            alt((Self::parse_week_format, Self::parse_datetime_format)),
+        ))(rest))
+        .or_else(|err| {
+            return Err(Self::Err::new(ErrorKind::DurationParse).with_msg(&format!("{}", err)));
+        })
+        .unwrap();
+
+        duration.sign = if let Some(sign) = sign {
+            if sign == '-' {
+                -1
+            } else {
+                1
+            }
+        } else {
+            1
+        };
+
+        Ok(duration)
     }
 }
 
@@ -35,14 +169,19 @@ impl TryFrom<&Property> for DurationSpec {
     type Error = super::error::Error;
 
     fn try_from(value: &Property) -> Result<Self, Self::Error> {
-        unimplemented!();
+        let val = value
+            .value
+            .as_ref()
+            .ok_or(Self::Error::from(ErrorKind::EventParse).with_msg("Empty duration property"))?;
+
+        val.parse::<Self>()
     }
 }
 
 #[derive(Clone, PartialEq, Eq)]
 pub enum TimeSpan {
     TimePoints(DateTimeSpec, DateTimeSpec),
-    Duration(DateTimeSpec, chrono::Duration),
+    Duration(DateTimeSpec, DurationSpec),
 }
 
 impl TimeSpan {
@@ -50,7 +189,7 @@ impl TimeSpan {
         TimeSpan::TimePoints(begin, end)
     }
 
-    pub fn from_start_and_duration(begin: DateTimeSpec, end: chrono::Duration) -> Self {
+    pub fn from_start_and_duration(begin: DateTimeSpec, end: DurationSpec) -> Self {
         TimeSpan::Duration(begin, end)
     }
 
@@ -64,7 +203,7 @@ impl TimeSpan {
     pub fn end(&self) -> DateTimeSpec {
         match &self {
             TimeSpan::TimePoints(_, end) => end.clone(),
-            TimeSpan::Duration(begin, dur) => begin.clone().and_duration(dur.clone()),
+            TimeSpan::Duration(begin, dur) => begin.clone().and_duration(dur.as_chrono_duration()),
         }
     }
 }
@@ -152,12 +291,16 @@ impl TryFrom<&IcalEvent> for OccurrenceSpec {
             )));
         };
 
-        // // Check if DURATION is set
-        // let duration = value.properties.iter().find(|p| p.name == "DURATION");
+        // Check if DURATION is set
+        let duration = value.properties.iter().find(|p| p.name == "DURATION");
 
-        // if let Some(duration) = duration {
-        //     Ok(OccurrenceSpec::Onetime(TimeSpan::from_start_and_duration(dtstart_spec, duration)
-        // };
+        if let Some(duration) = duration {
+            let dur_spec = DurationSpec::try_from(duration)?;
+            return Ok(OccurrenceSpec::Onetime(TimeSpan::from_start_and_duration(
+                dtstart_spec,
+                dur_spec,
+            )));
+        };
 
         // If neither DTEND, nor DURATION is specified event duration depends solely
         // on DTSTART. RFC 5545 states, that if DTSTART is...
