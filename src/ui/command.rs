37d60a98694332366f783e5c34947111ebcc8f43
reedts <j.reedts@gmail.com>
2022-02-06 13:39:22 +0100

    Add jump commands for command mode

--- src/ui/command.rs
+++ src/ui/command.rs
@@ -5,7 +5,16 @@ use unsegen::input::*;
 use unsegen::widget::builtin::PromptLine;
 
 use chrono::{Duration, NaiveDateTime};
-use nom::{branch::alt, combinator::all_consuming, error::Error, IResult};
+
+use nom::{
+    branch::alt,
+    bytes::complete::*,
+    character::complete::*,
+    combinator::*,
+    error::{Error, ErrorKind, ParseError},
+    sequence::{pair, separated_pair, tuple},
+    Err, IResult,
+};
 
 use super::context::{Context, Mode};
 use crate::config::Config;
@@ -15,17 +24,104 @@ pub struct CommandParser<'a, 'c> {
     config: &'a Config,
 }
 
+fn match_one_of<'a, 's, T: ?Sized, Act: 's>(c: &'a T) -> impl Fn(&str) -> IResult<&str, &str> + 'a
+where
+    &'a T: IntoIterator<Item = &'s (&'s str, Act)>,
+{
+    move |input| {
+        for (name, _) in c {
+            match all_consuming(tag(name.as_bytes()))(input) {
+                done @ IResult::Ok(..) => return done,
+                _ => (),
+            }
+        }
+
+        Err(Err::Failure(ParseError::from_error_kind(
+            input,
+            ErrorKind::Tag,
+        )))
+    }
+}
+
 impl<'a, 'c> CommandParser<'a, 'c> {
     pub fn new(context: &'a mut Context<'c>, config: &'a Config) -> Self {
         CommandParser { context, config }
     }
 
-    pub fn run_command(&mut self, cmd: &str) -> Result<(), Box<dyn std::error::Error>> {
-        Ok(())
+    fn run_repeatable_command(&mut self, repeat: &str, cmd: &str) -> Result<(), Error<String>> {
+        let repeats = if repeat.is_empty() {
+            1
+        } else {
+            u32::from_str_radix(repeat, 10)
+                .or_else::<Error<String>, _>(|err| {
+                    return Err(ParseError::from_error_kind(repeat.into(), ErrorKind::Digit));
+                })
+                .unwrap()
+        };
+
+        let (_, act) = COMMANDS
+            .iter()
+            .find(|(c, a)| c == &cmd)
+            .ok_or(ParseError::from_error_kind(cmd.into(), ErrorKind::OneOf))?;
+
+        if let Action::Repeatable(a) = act {
+            a(self.context, repeats)
+        } else {
+            Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag))
+        }
     }
 
-    fn report_error(&mut self, error: Box<dyn std::error::Error>) {
+    fn run_nonargs_command(&mut self, cmd: &str) -> Result<(), Error<String>> {
+        let (_, act) = COMMANDS
+            .iter()
+            .find(|(c, _)| c == &cmd)
+            .ok_or(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag))?;
+
+        match act {
+            Action::NoArg(a) => a(self.context),
+            Action::Repeatable(a) => a(self.context, 1),
+            _ => Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag)),
+        }
+    }
+
+    fn run_arg_command(&mut self, cmd: &str, arg: &str) -> Result<(), Error<String>> {
+        let (_, act) = COMMANDS
+            .iter()
+            .find(|(c, _)| c == &cmd)
+            .ok_or(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag))?;
+
+        if let Action::Arg(a) = act {
+            a(self.context, arg.to_owned())
+        } else {
+            Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag))
+        }
+    }
+
+    pub fn run_command(&mut self, cmd: &str) -> Result<(), Error<String>> {
+        let res = all_consuming(tuple((digit1, Self::parse_command_code)))(cmd);
+
+        if let Ok((_, (repeats, code))) = res {
+            return self.run_repeatable_command(repeats, code);
+        };
+
+        let res = all_consuming(separated_pair(Self::parse_command_code, space1, rest))(cmd);
+
+        if let Ok((_, (code, arg))) = res {
+            return self.run_arg_command(code, arg);
+        }
+
+        let (_, cmd) = all_consuming(Self::parse_command_code)(cmd)
+            .or_else(|_| Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag)))?;
+
+        self.run_nonargs_command(cmd)
+    }
+
+    fn report_error(&mut self, error: Error<String>) {
         self.context.last_error_message = Some(format!("{}", error));
+    }
+
+    fn parse_command_code(input: &str) -> IResult<&str, &str> {
+        match_one_of(COMMANDS)(input)
     }
 }
 
