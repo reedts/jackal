59809fb980aceb239f5cfe9df5bef0c4d95336b0
reedts <j.reedts@gmail.com>
2022-08-16 11:45:11 +0200

    refactor!: simplify coammdn parsing

--- src/ui/command.rs
+++ src/ui/command.rs
@@ -24,22 +24,21 @@ pub struct CommandParser<'a, 'c> {
     config: &'a Config,
 }
 
-fn match_one_of<'a, 's, T: ?Sized, Act: 's>(c: &'a T) -> impl Fn(&str) -> IResult<&str, &str> + 'a
+pub fn match_action<'a, 's, T: ?Sized, Act: 's>(
+    c: &'a T,
+) -> impl Fn(&str) -> IResult<&str, (&'s str, &'s Act)> + 'a
 where
     &'a T: IntoIterator<Item = &'s (&'s str, Act)>,
 {
     move |input| {
-        for (name, _) in c {
-            match all_consuming(tag(name.as_bytes()))(input) {
-                done @ IResult::Ok(..) => return done,
-                _ => (),
-            }
+        if let Some((name, act)) = c.into_iter().find(|(name, _)| name == &input) {
+            Ok(("", (name, act)))
+        } else {
+            Err(Err::Failure(ParseError::from_error_kind(
+                input,
+                ErrorKind::Tag,
+            )))
         }
-
-        Err(Err::Failure(ParseError::from_error_kind(
-            input,
-            ErrorKind::Tag,
-        )))
     }
 }
 
@@ -48,34 +47,37 @@ impl<'a, 'c> CommandParser<'a, 'c> {
         CommandParser { context, config }
     }
 
-    fn run_repeatable_command(&mut self, repeat: &str, cmd: &str) -> Result<(), Error<String>> {
-        let repeats = if repeat.is_empty() {
-            1
-        } else {
-            u32::from_str_radix(repeat, 10)
-                .or_else::<Error<String>, _>(|_| {
-                    return Err(ParseError::from_error_kind(repeat.into(), ErrorKind::Digit));
-                })
-                .unwrap()
+    pub fn run_command(&mut self, cmd: &str) -> Result<(), Error<String>> {
+        let res = all_consuming(tuple((digit1, match_action(COMMANDS))))(cmd);
+
+        if let Ok((_, (repeat, (_, act)))) = res {
+            let repeats = if repeat.is_empty() {
+                1
+            } else {
+                u32::from_str_radix(repeat, 10)
+                    .or_else::<Error<String>, _>(|_| {
+                        return Err(ParseError::from_error_kind(repeat.into(), ErrorKind::Digit));
+                    })
+                    .unwrap()
+            };
+
+            if let Action::Repeatable(a) = act {
+                return a(self.context, repeats);
+            }
         };
 
-        let (_, act) = COMMANDS
-            .iter()
-            .find(|(c, _)| c == &cmd)
-            .ok_or(ParseError::from_error_kind(cmd.into(), ErrorKind::OneOf))?;
+        let res = all_consuming(separated_pair(match_action(COMMANDS), space1, rest))(cmd);
 
-        if let Action::Repeatable(a) = act {
-            a(self.context, repeats)
-        } else {
-            Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag))
-        }
-    }
+        if let Ok((_, ((_, act), arg))) = res {
+            if let Action::Arg(a) = act {
+                return a(self.context, arg.to_owned());
+            } else {
+                return Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag));
+            }
+        };
 
-    fn run_nonargs_command(&mut self, cmd: &str) -> Result<(), Error<String>> {
-        let (_, act) = COMMANDS
-            .iter()
-            .find(|(c, _)| c == &cmd)
-            .ok_or(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag))?;
+        let (_, (cmd, act)) = all_consuming(match_action(COMMANDS))(cmd)
+            .or_else(|_| Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag)))?;
 
         match act {
             Action::NoArg(a) => a(self.context),
@@ -84,44 +86,8 @@ impl<'a, 'c> CommandParser<'a, 'c> {
         }
     }
 
-    fn run_arg_command(&mut self, cmd: &str, arg: &str) -> Result<(), Error<String>> {
-        let (_, act) = COMMANDS
-            .iter()
-            .find(|(c, _)| c == &cmd)
-            .ok_or(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag))?;
-
-        if let Action::Arg(a) = act {
-            a(self.context, arg.to_owned())
-        } else {
-            Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag))
-        }
-    }
-
-    pub fn run_command(&mut self, cmd: &str) -> Result<(), Error<String>> {
-        let res = all_consuming(tuple((digit1, Self::parse_command_code)))(cmd);
-
-        if let Ok((_, (repeats, code))) = res {
-            return self.run_repeatable_command(repeats, code);
-        };
-
-        let res = all_consuming(separated_pair(Self::parse_command_code, space1, rest))(cmd);
-
-        if let Ok((_, (code, arg))) = res {
-            return self.run_arg_command(code, arg);
-        }
-
-        let (_, cmd) = all_consuming(Self::parse_command_code)(cmd)
-            .or_else(|_| Err(ParseError::from_error_kind(cmd.into(), ErrorKind::Tag)))?;
-
-        self.run_nonargs_command(cmd)
-    }
-
     fn report_error(&mut self, error: Error<String>) {
         self.context.last_error_message = Some(format!("{}", error));
-    }
-
-    fn parse_command_code(input: &str) -> IResult<&str, &str> {
-        match_one_of(COMMANDS)(input)
     }
 }
 
